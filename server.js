8d2861950c865366c3f475899006c2e62043092b
Jong-hwan <Jong-hwan@Jong-hwan-Macpro.local>
2020-05-13 20:17:48 +0900

    add to cors

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('hello world');
@@ -13,8 +16,12 @@ app.get('/page2', (req, res) => {
     res.send(str);
 })
 
-app.get('/manifest.mpd', (req, res) => {
+app.get('/manifest.mpd', (req, res, next) => {
+    // res.header("Access-Control-Allow-Origin", "*");
+    // res.header("Access-Control-Allow-Headers", "X-Requested-With");   
     res.sendFile(path.join(__dirname, '/manifest.mpd'));
+
+    next();
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
